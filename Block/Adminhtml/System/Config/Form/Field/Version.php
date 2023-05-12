b9c33791d3a5719c42d4081a90631406a33b2d95
andrii onufriichuk <andrii.onufriichuk@devico.io>
2023-05-12 13:13:41 +0300

    mend

--- Block/Adminhtml/System/Config/Form/Field/Version.php
+++ Block/Adminhtml/System/Config/Form/Field/Version.php
@@ -6,48 +6,27 @@ namespace Rvvup\Payments\Block\Adminhtml\System\Config\Form\Field;
 
 use Magento\Backend\Block\Template\Context;
 use Magento\Config\Block\System\Config\Form\Field;
-use Magento\Framework\Component\ComponentRegistrarInterface;
 use Magento\Framework\Data\Form\Element\AbstractElement;
-use Magento\Framework\Component\ComponentRegistrar;
-use Magento\Framework\Exception\FileSystemException;
-use Magento\Framework\Exception\ValidatorException;
-use Magento\Framework\Filesystem\Directory\ReadFactory;
-use Magento\Framework\Serialize\Serializer\Json;
+use Rvvup\Payments\Model\Environment\GetEnvironmentVersions;
 
 class Version extends Field
 {
     /**
-     * @var ComponentRegistrarInterface
+     * @var GetEnvironmentVersions
      */
-    private ComponentRegistrarInterface $componentRegistrar;
-
-    /**
-     * @var ReadFactory
-     */
-    private ReadFactory $readFactory;
-
-    /**
-     * @var Json
-     */
-    private Json $serializer;
+    private GetEnvironmentVersions $environmentVersions;
 
     /**
      * @param Context $context
-     * @param ComponentRegistrarInterface $componentRegistrar
-     * @param ReadFactory $readFactory
-     * @param Json $serializer
+     * @param GetEnvironmentVersions $environmentVersions
      * @param array $data
      */
     public function __construct(
         Context $context,
-        ComponentRegistrarInterface $componentRegistrar,
-        ReadFactory $readFactory,
-        Json $serializer,
+        GetEnvironmentVersions $environmentVersions,
         array $data = []
     ) {
-        $this->componentRegistrar = $componentRegistrar;
-        $this->readFactory = $readFactory;
-        $this->serializer = $serializer;
+        $this->environmentVersions = $environmentVersions;
         parent::__construct(
             $context,
             $data
@@ -57,69 +36,8 @@ class Version extends Field
     protected function _getElementHtml(AbstractElement $element)
     {
         $element->setDisabled('disabled');
-        $element->setValue($this->getRvvupPluginVersion());
+        $element->setValue($this->environmentVersions->getRvvupModuleVersion());
 
         return $element->getElementHtml();
-    }
-
-    /**
-     * @return string
-     * @throws FileSystemException
-     * @throws ValidatorException
-     */
-    public function getRvvupPluginVersion(): string
-    {
-        $path = $this->getPath();
-        $composerJsonData = $this->getComposerData($path);
-        return $this->getModuleVersion($composerJsonData);
-    }
-
-    /**
-     * @return string|null
-     */
-    protected function getPath(): ?string
-    {
-        return $this->componentRegistrar->getPath(
-            ComponentRegistrar::MODULE,
-            'Rvvup_Payments'
-        );
-    }
-
-    /**
-     * @param string $path
-     * @return string
-     * @throws FileSystemException
-     * @throws ValidatorException
-     */
-    protected function getComposerData(string $path): string
-    {
-        $directoryRead = $this->readFactory->create($path);
-        try {
-            return $directoryRead->readFile('composer.json');
-        } catch (\Exception $e) {
-            return '';
-        }
-    }
-
-    /**
-     * @param string $composerJsonData
-     * @return string
-     */
-    protected function getModuleVersion(string $composerJsonData): string
-    {
-        if (empty($composerJsonData)) {
-            return '';
-        }
-
-        $data = $this->getSerializer()->unserialize($composerJsonData);
-        return $data['version'] ?? '';
-    }
-
-    /**
-     * @return Json
-     */
-    protected function getSerializer(): Json
-    {
-        return $this->serializer;
     }
 }
