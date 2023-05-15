5007de4f390cd1f6ddb0ab5f2f2cf5314abef76e
andrii-onufriichuk <117644505+andrii-onufriichuk@users.noreply.github.com>
2023-05-15 14:36:15 +0300

    Added plugin version into admin configuration (#28)
    * Added plugin version into admin configuration

---
+++ Block/Adminhtml/System/Config/Form/Field/Version.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rvvup\Payments\Block\Adminhtml\System\Config\Form\Field;
+
+use Magento\Backend\Block\Template\Context;
+use Magento\Config\Block\System\Config\Form\Field;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+use Rvvup\Payments\Model\Environment\GetEnvironmentVersions;
+
+class Version extends Field
+{
+    /**
+     * @var GetEnvironmentVersions
+     */
+    private GetEnvironmentVersions $environmentVersions;
+
+    /**
+     * @param Context $context
+     * @param GetEnvironmentVersions $environmentVersions
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        GetEnvironmentVersions $environmentVersions,
+        array $data = []
+    ) {
+        $this->environmentVersions = $environmentVersions;
+        parent::__construct(
+            $context,
+            $data
+        );
+    }
+
+    protected function _getElementHtml(AbstractElement $element)
+    {
+        $element->setDisabled('disabled');
+        $element->setValue($this->environmentVersions->getRvvupModuleVersion());
+
+        return $element->getElementHtml();
+    }
+}
