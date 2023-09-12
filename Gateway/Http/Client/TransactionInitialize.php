91bbe006dc75dc034fd75151aa7e7324ee46ae51
valentin-rvvup <118286907+valentin-rvvup@users.noreply.github.com>
2023-09-12 16:42:28 +0300

    PLUGIN-423: Add domain monitoring on order creation (#55)
    * feat: Add domain monitoring on order creation
    
    * docs: Removed redundant comments
    
    * style: Fix formatting issues
    
    * fix: Use correct URL type and replace deprecated parse_url usage
    
    * fix: Replaced deprecated function usage
    
    * fix: Removed redundant parsing function

--- Gateway/Http/Client/TransactionInitialize.php
+++ Gateway/Http/Client/TransactionInitialize.php
@@ -4,9 +4,11 @@ namespace Rvvup\Payments\Gateway\Http\Client;
 
 use Exception;
 use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\UrlInterface;
 use Magento\Payment\Gateway\Http\ClientException;
 use Magento\Payment\Gateway\Http\ClientInterface;
 use Magento\Payment\Gateway\Http\TransferInterface;
+use Magento\Store\Model\StoreManagerInterface;
 use Psr\Log\LoggerInterface;
 use Rvvup\Payments\Exception\QuoteValidationException;
 use Rvvup\Payments\Gateway\Method;
@@ -33,18 +35,26 @@ class TransactionInitialize implements ClientInterface
     private $orderDataBuilder;
 
     /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
+
+    /**
      * @param SdkProxy $sdkProxy
      * @param LoggerInterface $logger
      * @param OrderDataBuilder $orderDataBuilder
+     * @param StoreManagerInterface $storeManager
      */
     public function __construct(
         SdkProxy $sdkProxy,
         LoggerInterface $logger,
-        OrderDataBuilder $orderDataBuilder
+        OrderDataBuilder $orderDataBuilder,
+        StoreManagerInterface $storeManager
     ) {
         $this->sdkProxy = $sdkProxy;
         $this->logger = $logger;
         $this->orderDataBuilder = $orderDataBuilder;
+        $this->storeManager = $storeManager;
     }
 
     /**
@@ -73,6 +83,13 @@ class TransactionInitialize implements ClientInterface
             }
 
             $input = $this->roundOrderValues($transferObject->getBody());
+
+            $secureBaseUrl = $this->storeManager->getStore()->getBaseUrl(
+                UrlInterface::URL_TYPE_WEB,
+                true
+            );
+            $input['metadata']['domain'] = $secureBaseUrl;
+
             $order = $this->sdkProxy->createOrder(['input' => $input]);
 
             if ($order['data']['orderCreate']['status'] == Method::STATUS_EXPIRED) {
@@ -98,6 +115,13 @@ class TransactionInitialize implements ClientInterface
     private function processExpiredOrder(string $orderId): array
     {
         $input = $this->orderDataBuilder->createInputForExpiredOrder($orderId);
+
+        $secureBaseUrl = $this->storeManager->getStore()->getBaseUrl(
+            UrlInterface::URL_TYPE_WEB,
+            true
+        );
+        $input['metadata']['domain'] = $secureBaseUrl;
+
         return $this->sdkProxy->createOrder(['input' => $input]);
     }
 
