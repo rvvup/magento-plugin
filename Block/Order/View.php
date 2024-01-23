7e643d13d1199cdeb6f9690ce4b781bc5f2aa815
andrii onufriichuk <andrii.onufriichuk@devico.io>
2024-01-23 15:50:32 +0200

    mend

--- Block/Order/View.php
+++ Block/Order/View.php
@@ -4,10 +4,45 @@ declare(strict_types=1);
 
 namespace Rvvup\Payments\Block\Order;
 
+use Klarna\Core\Api\OrderRepositoryInterface;
+use Magento\Checkout\Model\Session;
+use Magento\Framework\App\Http\Context;
 use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Registry;
+use Magento\Payment\Helper\Data;
+use Magento\Sales\Model\Order;
 
 class View extends \Magento\Sales\Block\Order\View
 {
+    /** @var Session */
+    private $session;
+
+    /** @var Order */
+    private $order;
+
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param Registry $registry
+     * @param Context $httpContext
+     * @param Data $paymentHelper
+     * @param Session $session
+     * @param OrderRepositoryInterface $orderRepository
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        Registry $registry,
+        Context $httpContext,
+        Data $paymentHelper,
+        Session $session,
+        Order $order,
+        array $data = []
+    ) {
+        $this->session = $session;
+        $this->order = $order;
+        parent::__construct($context, $registry, $httpContext, $paymentHelper, $data);
+    }
+
     /**
      * @inheritDoc
      * @return void
@@ -15,11 +50,17 @@ class View extends \Magento\Sales\Block\Order\View
      */
     protected function _prepareLayout(): void
     {
-        $this->pageConfig->getTitle()->set(__('Order # %1', $this->getOrder()->getRealOrderId()));
+        $orderId = $this->getOrder()->getRealOrderId();
         $payment = $this->getOrder()->getPayment();
-        if ($payment) {
-            $infoBlock = $this->_paymentHelper->getInfoBlock($payment, $this->getLayout());
-            $this->setChild('payment_info', $infoBlock);
+        if (!$orderId) {
+            $orderId = $this->session->getLastRealOrderId();
         }
+        if (!$payment) {
+            $order = $this->order->loadByIncrementId($orderId);
+            $payment = $order->getPayment();
+        }
+        $this->pageConfig->getTitle()->set(__('Order # %1', $orderId));
+        $infoBlock = $this->_paymentHelper->getInfoBlock($payment, $this->getLayout());
+        $this->setChild('payment_info', $infoBlock);
     }
 }
