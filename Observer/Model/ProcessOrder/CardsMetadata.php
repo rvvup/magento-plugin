eecb95d86dab1235249089716689485fd0471ce3
Andrii Onufriichuk <andrii.onufriichuk@rvvup.com>
2024-02-05 11:36:32 +0200

    mend

--- Observer/Model/ProcessOrder/CardsMetadata.php
+++ Observer/Model/ProcessOrder/CardsMetadata.php
@@ -78,7 +78,8 @@ class CardsMetadata implements ObserverInterface
                     $historyComment->setIsCustomerNotified(0);
                     $historyComment->setIsVisibleOnFront(0);
                     $historyComment->setStatus($order->getStatus());
-                    $message = __('Rvvup payment status %1, card data %2', $payment['status'], json_encode($data));
+                    $status = nl2br("Rvvup payment status " . $payment['status'] . "\n card data: \n");
+                    $message = __($status . nl2br(implode("\n", $data)));
                     $historyComment->setComment($message);
                     $this->orderManagement->addComment($order->getEntityId(), $historyComment);
                 } catch (\Exception $e) {
@@ -97,7 +98,35 @@ class CardsMetadata implements ObserverInterface
     private function populateCardData(array &$data, array $payment, string $key): void
     {
         if (isset($payment[$key])) {
-            $data[$key] = $payment[$key];
+            $value = $this->mapCardValue($payment[$key]);
+            $data[$key] = $key . ': ' . $value;
+        }
+    }
+
+    /**
+     * @param string $value
+     * @return string
+     */
+    private function mapCardValue(string $value): string
+    {
+        switch ($value) {
+            case "0":
+                if ($value !== '0') {
+                    return $value;
+                }
+                return '0 - Not Given';
+
+            case "1":
+                return '1 - Not Checked';
+
+            case "2":
+                return '2 - Match';
+
+            case "4":
+                return '4 - Not Matched';
+
+            default:
+                return $value;
         }
     }
 }
