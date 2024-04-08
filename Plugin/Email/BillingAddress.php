2ea17f2e2ac09dc60879e5c080cf3379825650ed
Andrii Onufriichuk <andrii.onufriichuk@rvvup.com>
2024-04-08 17:43:42 +0300

    Fix for billing address email

--- Plugin/Email/BillingAddress.php
+++ Plugin/Email/BillingAddress.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Rvvup\Payments\Plugin\Email;
 
 use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Quote\Api\BillingAddressManagementInterface;
 use Magento\Quote\Api\CartRepositoryInterface;
 use Magento\Quote\Api\Data\AddressInterface;
 use Magento\Quote\Model\BillingAddressManagement;
@@ -45,18 +46,15 @@ class BillingAddress
      * @throws NoSuchEntityException
      */
     public function beforeAssign(
-        BillingAddressManagement $subject,
-        ?int $cartId,
+        BillingAddressManagementInterface $subject,
+        int $cartId,
         AddressInterface $address,
-        bool $useForShipping  =  false
+        bool $useForShipping = false
     ): array {
-        if ($cartId) {
-            $quote = $this->quoteRepository->getActive($cartId);
-            $email = $quote->getBillingAddress()->getEmail();
-            if ($email && !$quote->getCustomerEmail()) {
-                $quote->setCustomerEmail($email);
-                $this->quoteRepository->save($quote);
-            }
+        $quote = $this->quoteRepository->getActive($cartId);
+        $email = $quote->getBillingAddress()->getEmail();
+        if ($email) {
+            $address->setEmail($email);
         }
 
         return [$cartId, $address, $useForShipping];
