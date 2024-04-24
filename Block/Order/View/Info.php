92aef50078db1c135c96eb9ffd0b3678bd3001b7
Andrii Onufriichuk <andrii.onufriichuk@rvvup.com>
2024-04-24 15:56:26 +0300

    Display rvvup moto only if enabled

--- Block/Order/View/Info.php
+++ Block/Order/View/Info.php
@@ -16,11 +16,15 @@ use Magento\Sales\Model\Order\Address\Renderer;
 use Magento\Store\Model\ScopeInterface;
 use Rvvup\Payments\Model\ConfigInterface;
 use Rvvup\Payments\Model\RvvupConfigProvider;
+use Rvvup\Payments\Model\SdkProxy;
 
 class Info extends MagentoInfo
 {
     /** @var ConfigInterface */
     private $config;
+
+    /** @var SdkProxy */
+    private $sdkProxy;
 
     /**
      * @param Context $context
@@ -31,6 +35,7 @@ class Info extends MagentoInfo
      * @param ElementFactory $elementFactory
      * @param Renderer $addressRenderer
      * @param ConfigInterface $config
+     * @param SdkProxy $sdkProxy
      * @param array $data
      */
     public function __construct(
@@ -42,9 +47,11 @@ class Info extends MagentoInfo
         ElementFactory $elementFactory,
         Renderer $addressRenderer,
         ConfigInterface $config,
+        SdkProxy $sdkProxy,
         array $data = []
     ) {
         $this->config = $config;
+        $this->sdkProxy = $sdkProxy;
         parent::__construct(
             $context,
             $registry,
@@ -61,17 +68,34 @@ class Info extends MagentoInfo
      * @return bool
      * @throws LocalizedException
      */
-    public function shouldDisplayMoto(): bool
+    public function shouldDisplayRvvup(): bool
     {
         if ($this->getOrder()) {
             $order = $this->getOrder();
             if ($this->config->isActive(ScopeInterface::SCOPE_STORE, $order->getStoreId())) {
                 $payment = $order->getPayment();
                 if ($payment && $payment->getMethod() == RvvupConfigProvider::CODE) {
-                    if ($this->getOrder()->getState() == Order::STATE_NEW) {
+                    if ($order->getState() == Order::STATE_NEW) {
                         return true;
                     }
                 }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @return bool
+     * @throws LocalizedException
+     */
+    public function isMotoAvailable(): bool
+    {
+        $amount = $this->getOrder()->getGrandTotal();
+        $currency = $this->getOrder()->getOrderCurrencyCode();
+        $methods = $this->sdkProxy->getMethods($amount, $currency);
+        foreach ($methods as $method) {
+            if (isset($method['settings']['motoEnabled']) && $method['settings']['motoEnabled']) {
+                return true;
             }
         }
         return false;
@@ -83,7 +107,7 @@ class Info extends MagentoInfo
      */
     public function getPaymentLink(): ?string
     {
-        if ($this->shouldDisplayMoto()) {
+        if ($this->shouldDisplayRvvup()) {
             $payment = $this->getOrder()->getPayment();
             $message = $payment->getAdditionalInformation('rvvup_payment_link_message');
             if ($message && is_string($message)) {
