da553f4381bfdb3a6cf4dd9b0e850c343bdb63b6
Valentin Stoykov <valentin@rvvup.com>
2024-04-29 13:49:08 +0100

    new: Can place an order using modal pay by card flow

--- Test/End-2-End/pay_by_card.spec.js
+++ Test/End-2-End/pay_by_card.spec.js
@@ -21,3 +21,27 @@ test('Can place an order using inline pay by card', async ({ page }) => {
 
     await expect(page.getByRole('heading', { name: 'Thank you for your purchase!' })).toBeVisible();
 });
+
+test.skip('Can place an order using modal pay by card', async ({ page }) => {
+    const visitCheckoutPayment = new VisitCheckoutPayment(page);
+    await visitCheckoutPayment.visit();
+
+    await page.getByLabel('Pay by Card').click();
+
+    await page.getByRole('button', { name: 'Place order' }).click();
+
+    // Credit card form
+    const frame = page.frameLocator('#rvvup_iframe-rvvup_CARD');
+    await frame.frameLocator('.st-card-number-iframe').getByLabel('Card Number').fill('4111 1111 1111 1111');
+    await frame.frameLocator('.st-expiration-date-iframe').getByLabel('Expiration Date').fill('1233');
+    await frame.frameLocator('.st-security-code-iframe').getByLabel('Security Code').fill('123');
+    await frame.getByRole('button', { name: 'Submit'}).click();
+
+    // OTP form
+    await frame.frameLocator('#Cardinal-CCA-IFrame').getByPlaceholder('Enter Code Here').fill('1234');
+    await frame.frameLocator('#Cardinal-CCA-IFrame').getByPlaceholder('Enter Code Here').press('Enter');
+
+    await page.waitForURL("./default/checkout/onepage/success/");
+
+    await expect(page.getByRole('heading', { name: 'Thank you for your purchase!' })).toBeVisible();
+});
