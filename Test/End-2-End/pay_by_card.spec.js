629f4beeb69d42c2c4f28ffa148d41330398f638
Valentin Stoykov <valentin@rvvup.com>
2024-04-29 14:16:17 +0100

    new: Can place frictionless 3DS card orders

--- Test/End-2-End/pay_by_card.spec.js
+++ Test/End-2-End/pay_by_card.spec.js
@@ -1,7 +1,7 @@
 import test, { expect } from "@playwright/test";
 import VisitCheckoutPayment from "./Pages/VisitCheckoutPayment";
 
-test('Can place an order using inline pay by card', async ({ page }) => {
+test('Can place an inline pay by card order with 3DS challenge', async ({ page }) => {
     const visitCheckoutPayment = new VisitCheckoutPayment(page);
     await visitCheckoutPayment.visit();
 
@@ -22,7 +22,24 @@ test('Can place an order using inline pay by card', async ({ page }) => {
     await expect(page.getByRole('heading', { name: 'Thank you for your purchase!' })).toBeVisible();
 });
 
-test.skip('Can place an order using modal pay by card', async ({ page }) => {
+test('Can place an inline pay by card order without 3DS challenge', async ({ page }) => {
+    const visitCheckoutPayment = new VisitCheckoutPayment(page);
+    await visitCheckoutPayment.visit();
+
+    await page.getByLabel('Pay by Card').click();
+
+    // Credit card form
+    await page.frameLocator('.st-card-number-iframe').getByLabel('Card Number').fill('4000 0000 0000 2701');
+    await page.frameLocator('.st-expiration-date-iframe').getByLabel('Expiration Date').fill('1233');
+    await page.frameLocator('.st-security-code-iframe').getByLabel('Security Code').fill('123');
+    await page.getByRole('button', { name: 'Place order' }).click();
+
+    await page.waitForURL("./default/checkout/onepage/success/");
+
+    await expect(page.getByRole('heading', { name: 'Thank you for your purchase!' })).toBeVisible();
+});
+
+test.skip('Can place a modal pay by card order with 3DS challenge', async ({ page }) => {
     const visitCheckoutPayment = new VisitCheckoutPayment(page);
     await visitCheckoutPayment.visit();
 
@@ -40,6 +57,26 @@ test.skip('Can place an order using modal pay by card', async ({ page }) => {
     // OTP form
     await frame.frameLocator('#Cardinal-CCA-IFrame').getByPlaceholder('Enter Code Here').fill('1234');
     await frame.frameLocator('#Cardinal-CCA-IFrame').getByPlaceholder('Enter Code Here').press('Enter');
+
+    await page.waitForURL("./default/checkout/onepage/success/");
+
+    await expect(page.getByRole('heading', { name: 'Thank you for your purchase!' })).toBeVisible();
+});
+
+test.skip('Can place a modal pay by card order without 3DS challenge', async ({ page }) => {
+    const visitCheckoutPayment = new VisitCheckoutPayment(page);
+    await visitCheckoutPayment.visit();
+
+    await page.getByLabel('Pay by Card').click();
+
+    await page.getByRole('button', { name: 'Place order' }).click();
+
+    // Credit card form
+    const frame = page.frameLocator('#rvvup_iframe-rvvup_CARD');
+    await frame.frameLocator('.st-card-number-iframe').getByLabel('Card Number').fill('4000 0000 0000 2701');
+    await frame.frameLocator('.st-expiration-date-iframe').getByLabel('Expiration Date').fill('1233');
+    await frame.frameLocator('.st-security-code-iframe').getByLabel('Security Code').fill('123');
+    await frame.getByRole('button', { name: 'Submit'}).click();
 
     await page.waitForURL("./default/checkout/onepage/success/");
 
