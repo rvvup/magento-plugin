08ca0264d6546d3a6e90e92bdb6814345cb20407
Aashwin Mohan <96258159+aashwin-rvvup@users.noreply.github.com>
2024-05-09 16:42:49 +0100

    Update test urls (#120)

--- run-e2e-tests.sh
+++ run-e2e-tests.sh
@@ -3,7 +3,7 @@ start=$(date +%s)
 docker compose up -d --build
 attempt=1
 while true; do
-    http_status=$(curl -o /dev/null -s -w "%{http_code}\n" -I "http://localhost/magento_version")
+    http_status=$(curl -o /dev/null -s -w "%{http_code}\n" -I "http://local.dev.rvvuptech.com/magento_version")
 
     if [ "$http_status" -eq 200 ]; then
         echo -e "\rServer responded with 200 OK / Time taken: $(($(date +%s) - start)) seconds, continuing..."
@@ -16,7 +16,7 @@ while true; do
 done
 
 if [ "$1" == "--ui" ]; then
-    ENV TEST_BASE_URL=http://localhost npx playwright test --ui
+    ENV TEST_BASE_URL=http://local.dev.rvvuptech.com npx playwright test --ui
 else
-    ENV TEST_BASE_URL=http://localhost npx playwright test
+    ENV TEST_BASE_URL=http://local.dev.rvvuptech.com npx playwright test
 fi
