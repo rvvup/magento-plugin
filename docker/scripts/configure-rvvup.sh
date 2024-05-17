e7a97c9a9f931c49bc3e27dbe05571133335267a
Aashwin Mohan <aashwin@rvvup.com>
2024-05-17 16:00:15 +0100

    Example of using wiremock to proxy

--- docker/scripts/configure-rvvup.sh
+++ docker/scripts/configure-rvvup.sh
@@ -9,5 +9,20 @@ bin/magento setup:di:compile
 bin/magento setup:static-content:deploy -f
 bin/magento cache:flush
 
-bin/magento config:set payment/rvvup/jwt $RVVUP_API_KEY
+HEADER=$(echo "$RVVUP_API_KEY" | cut -d '.' -f 1)
+PAYLOAD=$(echo "$RVVUP_API_KEY" | cut -d '.' -f 2)
+SIGNATURE=$(echo "$RVVUP_API_KEY" | cut -d '.' -f 3)
+PAYLOAD_LEN=$((${#PAYLOAD} % 4))
+    if [ $PAYLOAD_LEN -eq 2 ]; then
+        PAYLOAD="${PAYLOAD}=="
+    elif [ $PAYLOAD_LEN -eq 3 ]; then
+        PAYLOAD="${PAYLOAD}="
+    fi
+DECODED_PAYLOAD=$(echo "$PAYLOAD" | base64 --decode)
+
+UPDATED_PAYLOAD=$(echo "$DECODED_PAYLOAD" | jq '.aud = "http://wiremock:8080/graphql"')
+ENCODED_PAYLOAD=$(echo -n "$UPDATED_PAYLOAD" | base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n')
+RVVUP_KEY_TO_USE="${HEADER}.${ENCODED_PAYLOAD}.test"
+
+bin/magento config:set payment/rvvup/jwt $RVVUP_KEY_TO_USE
 bin/magento config:set payment/rvvup/active 1
