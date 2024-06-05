10f80faea9ab5d3a4a9327b670704676c3d40e6d
Andrii Onufriichuk <andrii.onufriichuk@rvvup.com>
2024-06-05 10:53:18 +0300

    Changed dashboard link functionality

--- Block/Info.php
+++ Block/Info.php
@@ -2,7 +2,11 @@
 
 namespace Rvvup\Payments\Block;
 
+use Magento\Framework\View\Element\Template\Context;
 use Magento\Payment\Block\ConfigurableInfo;
+use Magento\Payment\Gateway\ConfigInterface;
+use Magento\Quote\Api\CartRepositoryInterface;
+use Magento\Sales\Api\Data\OrderPaymentInterface;
 
 class Info extends ConfigurableInfo
 {
@@ -16,6 +20,29 @@ class Info extends ConfigurableInfo
      * @var string
      */
     protected $_template = 'Rvvup_Payments::info/default.phtml';
+
+    /** @var CartRepositoryInterface */
+    private $cartRepository;
+
+    /**
+     * @param Context $context
+     * @param ConfigInterface $config
+     * @param CartRepositoryInterface $cartRepository
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        ConfigInterface $config,
+        CartRepositoryInterface $cartRepository,
+        array $data = []
+    ) {
+        $this->cartRepository = $cartRepository;
+        parent::__construct(
+            $context,
+            $config,
+            $data
+        );
+    }
 
     /**
      * @var string[]
@@ -32,5 +59,20 @@ class Info extends ConfigurableInfo
     protected function getLabel($field)
     {
         return $this->labels[$field] ?? $field;
+    }
+
+    public function getAdditionalInformation(string $field): string
+    {
+       $payment = $this->getInfo();
+       if ($payment->getAdditionalInformation($field)) {
+           return $payment->getAdditionalInformation($field) ?: '';
+       } elseif ($this->getInfo() instanceof OrderPaymentInterface) {
+         $quoteId = $payment->getOrder()->getQuoteId();
+         if ($quoteId) {
+             $cart = $this->cartRepository->get((int) $quoteId);
+             return $cart->getPayment()->getAdditionalInformation($field) ?: '';
+         }
+       }
+       return '';
     }
 }
