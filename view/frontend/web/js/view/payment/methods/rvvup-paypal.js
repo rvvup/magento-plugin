174eff90b858a98009b2cbccc615cd6183a272f7
Aashwin Mohan <aashwin@rvvup.com>
2024-07-26 15:06:35 +0100

    Add Firecheckout Compatibility when validating for Paypal

--- view/frontend/web/js/view/payment/methods/rvvup-paypal.js
+++ view/frontend/web/js/view/payment/methods/rvvup-paypal.js
@@ -2,6 +2,26 @@ define([], function () {
     'use strict';
 
     return function () {
+        const fireCheckoutModule = function () {
+            if (!checkoutConfig || !checkoutConfig.isFirecheckout) {
+                return null;
+            }
+            try {
+                return {
+                    validator: require('Swissup_Firecheckout/js/model/validator'),
+                    layout: require('Swissup_Firecheckout/js/model/layout')
+                };
+            } catch (e) {
+                return null;
+            }
+        };
+        const fireCheckoutValidation = function (fireCheckoutModule) {
+            if (!fireCheckoutModule.validator.validateShippingAddress()) {
+                return false;
+            }
+
+            return fireCheckoutModule.validator.validateShippingRadios();
+        };
         return {
             validate: function (main, additionalValidators) {
                 if (!main.validate()) {
@@ -14,9 +34,14 @@ define([], function () {
                 if (main.isPlaceOrderActionAllowed() === false) {
                     return false;
                 }
-               
+                //Swiss FireCheckout compatibility
+                const fcModule = fireCheckoutModule();
+                if (fcModule !== null) {
+                    return fireCheckoutValidation(fcModule)
+                }
                 return true;
-            },
+
+            }
         }
     }();
 });
