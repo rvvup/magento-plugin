c638cb67e2d8ef456e4f6ed9503a5dc38648ce73
Andrii Onufriichuk <andrii.onufriichuk@rvvup.com>
2024-08-13 15:26:30 +0300

    Revert "Added email saving to address if missing"
    This reverts commit 102020594c1e4ce33c12d0347a4cee5fc0d12e67.

--- Model/OrderDataBuilder.php
+++ Model/OrderDataBuilder.php
@@ -18,7 +18,6 @@ use Magento\Quote\Model\QuoteValidator;
 use Magento\Quote\Model\ResourceModel\Quote\Payment;
 use Magento\Sales\Api\Data\OrderInterface;
 use Magento\Sales\Api\OrderRepositoryInterface;
-use Magento\Sales\Model\ResourceModel\Order\Address;
 use Rvvup\Payments\Exception\QuoteValidationException;
 use Rvvup\Payments\Gateway\Method;
 
@@ -48,9 +47,6 @@ class OrderDataBuilder
     /** @var QuoteValidator */
     private $quoteValidator;
 
-    /** @var Address */
-    private $addressResource;
-
     /**
      * @param AddressRepositoryInterface $customerAddressRepository
      * @param UrlInterface $urlBuilder
@@ -59,7 +55,6 @@ class OrderDataBuilder
      * @param OrderRepositoryInterface $orderRepository
      * @param SearchCriteriaBuilder $searchCriteriaBuilder
      * @param QuoteValidator $quoteValidator
-     * @param Address $addressResource
      * @param Payment $paymentResource
      */
     public function __construct(
@@ -70,7 +65,6 @@ class OrderDataBuilder
         OrderRepositoryInterface $orderRepository,
         SearchCriteriaBuilder $searchCriteriaBuilder,
         QuoteValidator $quoteValidator,
-        Address $addressResource,
         Payment $paymentResource
     ) {
         $this->customerAddressRepository = $customerAddressRepository;
@@ -81,7 +75,6 @@ class OrderDataBuilder
         $this->searchCriteriaBuilder = $searchCriteriaBuilder;
         $this->quoteValidator = $quoteValidator;
         $this->paymentResource = $paymentResource;
-        $this->addressResource = $addressResource;
     }
 
     /**
@@ -109,12 +102,6 @@ class OrderDataBuilder
 
         $orderDataArray = $this->renderBase($quote, $express);
         $orderDataArray['customer'] = $this->renderCustomer($quote, $express, $billingAddress);
-        $email = $orderDataArray['customer']['email'];
-        if ($email && !$billingAddress->getEmail()) {
-            $billingAddress->setEmail($email);
-            $this->addressResource->save($billingAddress);
-        }
-
         $orderDataArray['billingAddress'] = $this->renderBillingAddress($quote, $express, $billingAddress);
 
         // We do not require shipping data for virtual orders (orders without tangible items).
@@ -123,11 +110,6 @@ class OrderDataBuilder
         }
 
         $shippingAddress = $quote->getShippingAddress();
-
-        if ($email && !$shippingAddress->getEmail()) {
-            $shippingAddress->setEmail($email);
-            $this->addressResource->save($shippingAddress);
-        }
 
         // Validate that Shipping Address exists if this is NOT a request to build express payment data.
         if (!$express && $shippingAddress === null) {
