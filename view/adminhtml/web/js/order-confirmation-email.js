2c35500c133d55e60173a32e85951ea9caebecfd
Andrii Onufriichuk <andrii.onufriichuk@rvvup.com>
2024-08-28 17:52:12 +0300

    Disabled sending emails for rvvup admin orders

--- view/adminhtml/web/js/order-confirmation-email.js
+++ view/adminhtml/web/js/order-confirmation-email.js
@@ -7,6 +7,11 @@ define(['jquery'], function($) {
             if (checkbox) {
                 checkbox.value = '0';
                 checkbox.checked = false;
+                checkbox.disabled = true;
+                var element = ($('label[for="send_confirmation"]'));
+                if (element && element[0]) {
+                    element[0].innerText = 'Order Confirmation email is always sent for Rvvup payment methods';
+                }
             }
         }
     };
