c8fc2ed039f00dce4fe77c07bedd5bc99b48ef63
Andrii Onufriichuk <andrii.onufriichuk@rvvup.com>
2024-08-29 16:08:51 +0300

    Remove secureRenderer check as we always support it

--- view/adminhtml/web/js/order-confirmation-email.js
+++ view/adminhtml/web/js/order-confirmation-email.js
@@ -8,10 +8,8 @@ define(['jquery'], function($) {
                 checkbox.value = '0';
                 checkbox.checked = false;
                 checkbox.disabled = true;
-                var element = ($('label[for="send_confirmation"]'));
-                if (element && element[0]) {
-                    element[0].innerText = 'Order Confirmation email is always sent for Rvvup payment methods';
-                }
+                $('label[for="send_confirmation"]').first()
+                    .text("Order Confirmation email is always sent for Rvvup payment methods");
             }
         }
     };
