7fbecfc4a7c392e9a78cbd7a9c91d34ecabea196
Aashwin Mohan <aashwin@rvvup.com>
2025-01-16 15:39:26 +0000

    Capturing payment via Invoice on Virtual Terminal

---
+++ src/Gateway/AdminPaymentMethod.php
@@ -0,0 +1,88 @@
+<?php declare(strict_types=1);
+
+namespace Rvvup\Payments\Gateway;
+
+use Magento\Framework\Event\ManagerInterface;
+use Magento\Payment\Block\Form;
+use Magento\Payment\Gateway\Command\CommandManagerInterface;
+use Magento\Payment\Gateway\Command\CommandPoolInterface;
+use Magento\Payment\Gateway\Config\ValueHandlerPoolInterface;
+use Magento\Payment\Gateway\Data\PaymentDataObjectFactory;
+use Magento\Payment\Gateway\Validator\ResultInterfaceFactory;
+use Magento\Payment\Gateway\Validator\ValidatorPoolInterface;
+use Magento\Payment\Model\Method\Adapter;
+use Magento\Store\Model\StoreManagerInterface;
+use Psr\Log\LoggerInterface;
+use Throwable;
+
+class AdminPaymentMethod extends Adapter
+{
+    /** @var string */
+    private $title;
+
+    /**
+     * @param ManagerInterface $eventManager
+     * @param ValueHandlerPoolInterface $valueHandlerPool
+     * @param PaymentDataObjectFactory $paymentDataObjectFactory
+     * @param string $code
+     * @param string $formBlockType
+     * @param string $infoBlockType
+     * @param LoggerInterface $logger // Set via di.xml
+     * @param string|null $title
+     * @param CommandPoolInterface|null $commandPool
+     * @param ValidatorPoolInterface|null $validatorPool
+     * @param CommandManagerInterface|null $commandExecutor
+     */
+    public function __construct(
+        ManagerInterface $eventManager,
+        ValueHandlerPoolInterface $valueHandlerPool,
+        PaymentDataObjectFactory $paymentDataObjectFactory,
+        string $code,
+        string $formBlockType,
+        string $infoBlockType,
+        LoggerInterface $logger,
+        ?string $title = null,
+        CommandPoolInterface $commandPool = null,
+        ValidatorPoolInterface $validatorPool = null,
+        CommandManagerInterface $commandExecutor = null,
+    ) {
+        parent::__construct(
+            $eventManager,
+            $valueHandlerPool,
+            $paymentDataObjectFactory,
+            $code,
+            $formBlockType,
+            $infoBlockType,
+            $commandPool,
+            $validatorPool,
+            $commandExecutor,
+            $logger
+        );
+
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title ?: parent::getTitle();
+    }
+
+    public function canUseInternal(): bool
+    {
+        return true;
+    }
+
+    /** @inheritDoc */
+    public function validate()
+    {
+        return $this;
+    }
+
+    public function getFormBlockType()
+    {
+        return Form::class;
+    }
+}
--- src/Gateway/Command/Capture.php
+++ src/Gateway/Command/Capture.php
@@ -4,64 +4,85 @@ declare(strict_types=1);
 
 namespace Rvvup\Payments\Gateway\Command;
 
+use Exception;
 use Magento\Payment\Gateway\Command\CommandException;
 use Magento\Payment\Gateway\CommandInterface;
-use Magento\Sales\Model\Order;
 use Magento\Sales\Model\Order\Payment;
 use Rvvup\Payments\Gateway\Method;
-use Rvvup\Payments\Model\RvvupConfigProvider;
+use Rvvup\Payments\Model\Logger;
+use Rvvup\Payments\Model\SdkProxy;
 use Rvvup\Sdk\Exceptions\NetworkException;
 
-class Capture extends AbstractCommand implements CommandInterface
+class Capture implements CommandInterface
 {
+
+    /** @var SdkProxy */
+    protected $sdkProxy;
+    /** @var Logger */
+    protected $logger;
+
+    /**
+     * @param SdkProxy $sdkProxy
+     * @param Logger $logger
+     */
+    public function __construct(
+        SdkProxy $sdkProxy,
+        Logger   $logger
+    )
+    {
+        $this->sdkProxy = $sdkProxy;
+        $this->logger = $logger;
+    }
+
     /**
      * @param array $commandSubject
-     * @return \Magento\Payment\Gateway\Command\ResultInterface|void|null
+     * @return void
+     * @throws Exception
      */
     public function execute(array $commandSubject)
     {
+        $this->logger->error('Capture command executed');
         /** @var Payment $payment */
         $payment = $commandSubject['payment']->getPayment();
-        /** @var Order $order */
-        $order = $payment->getOrder();
-        $storeId = (string)$order->getStoreId();
-        $paymentId = $payment->getAdditionalInformation(Method::ORDER_ID);
-        $rvvupOrder = $this->sdkProxy->getOrder($paymentId, $storeId);
-        $state = self::STATE_MAP[$rvvupOrder['payments'][0]['status']] ?? 'decline';
-        $this->$state($payment);
-    }
-
-    /**
-     * @param Payment $payment
-     * @return void
-     * @throws NetworkException
-     */
-    private function success(Payment $payment): void
-    {
-        /** @var Order $order */
         $order = $payment->getOrder();
         $storeId = (string)$order->getStoreId();
         $rvvupOrderId = $payment->getAdditionalInformation(Method::ORDER_ID);
-        $rvvupPaymentId = $payment->getAdditionalInformation(Method::PAYMENT_ID);
-        $this->sdkProxy->paymentCapture($rvvupOrderId, $rvvupPaymentId, $storeId);
+        if (!$rvvupOrderId) {
+            $this->logger->addRvvupError('Cannot capture online without rvvup order id', null, null, null, $order->getId());
+            throw new CommandException(__("Payment cannot be captured because it hasn't been authorised yet."));
+        }
+        $rvvupOrder = $this->sdkProxy->getOrder($rvvupOrderId, $storeId);
+        $rvvupPaymentId = $rvvupOrder['payments'][0]['id'];
+
+        switch ($rvvupOrder['payments'][0]['status']) {
+            case 'REQUIRES_ACTION':
+            case 'PENDING':
+                $payment->setIsTransactionPending(true);
+                break;
+            case 'SUCCEEDED':
+            case 'AUTHORIZED':
+                $this->success((string)$order->getStoreId(), $rvvupOrderId, $rvvupPaymentId, $payment);
+                break;
+            case 'CANCELLED':
+            case 'DECLINED':
+            default:
+                throw new CommandException(__('Sorry, the payment was declined, please try another method'));
+        }
     }
 
     /**
+     * @param string $storeId
+     * @param string $rvvupOrderId
+     * @param string $rvvupPaymentId
      * @param Payment $payment
      * @return void
+     * @throws NetworkException
+     * @throws \JsonException
      */
-    private function defer(Payment $payment): void
+    private function success(string $storeId, string $rvvupOrderId, string $rvvupPaymentId, Payment $payment): void
     {
-        $payment->setIsTransactionPending(true);
-    }
+        $this->logger->error('exec success');
 
-    /**
-     * @param Payment $payment
-     * @return mixed
-     * @throws CommandException
-     */
-    private function decline(Payment $payment)
-    {
-        throw new CommandException(__('Sorry, the payment was declined, please try another method'));
+        $this->sdkProxy->paymentCapture($rvvupOrderId, $rvvupPaymentId, $storeId);
     }
 }
