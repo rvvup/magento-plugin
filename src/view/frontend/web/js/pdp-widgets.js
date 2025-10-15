2d29776de43b55e29e431776f9822fa8bc04a21e
Aashwin Mohan <aashwin@rvvup.com>
2025-10-15 15:16:50 +0100

    Enable/Disable Messaging for ZRF on product page

--- src/view/frontend/web/js/pdp-widgets.js
+++ src/view/frontend/web/js/pdp-widgets.js
@@ -26,6 +26,9 @@ define([
             if (this.initialPrice === null) {
                 return this;
             }
+            if (rvvup_parameters.settings.zopa_retail_finance.product.widget.enabled !== true) {
+                return this;
+            }
             this.render();
 
         },
