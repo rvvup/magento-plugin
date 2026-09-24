4fb07fe1b68b083cba025a3eae96b66abad60cfc
igor-durmic-rvvup-zopa <igor.durmic@zopa.com>
2026-09-24 15:41:37 +0200

    Wire paymentSessions() and paymentMethodTokens() through ApiProvider

--- src/Service/ApiProvider.php
+++ src/Service/ApiProvider.php
@@ -3,8 +3,11 @@
 namespace Rvvup\Payments\Service;
 
 use Exception;
+use GuzzleHttp\Client;
 use Rvvup\Payments\Model\Config\RvvupConfigurationInterface;
 use Rvvup\Payments\Model\UserAgentBuilder;
+use Rvvup\Payments\Sdk\Rest\PaymentMethodTokens;
+use Rvvup\Payments\Sdk\Rest\PaymentSessions;
 use Rvvup\Sdk\Rest\Options\RvvupClientOptions;
 use Rvvup\Sdk\Rest\RvvupClient;
 
@@ -18,17 +21,25 @@ class ApiProvider
     /** @var array */
     private $clients;
 
+    /** @var array */
+    private $paymentSessionsWrappers;
+
+    /** @var array */
+    private $paymentMethodTokensWrappers;
+
     /**
      * @param RvvupConfigurationInterface $config
      * @param UserAgentBuilder $userAgent
      */
     public function __construct(
-        RvvupConfigurationInterface     $config,
+        RvvupConfigurationInterface $config,
         UserAgentBuilder $userAgent
     ) {
         $this->config = $config;
         $this->userAgent = $userAgent;
         $this->clients = [];
+        $this->paymentSessionsWrappers = [];
+        $this->paymentMethodTokensWrappers = [];
     }
 
     /**
@@ -37,6 +48,8 @@ class ApiProvider
     public function clean()
     {
         $this->clients = [];
+        $this->paymentSessionsWrappers = [];
+        $this->paymentMethodTokensWrappers = [];
     }
 
     /**
@@ -53,5 +66,45 @@ class ApiProvider
             );
         }
         return $this->clients[$storeId];
+    }
+
+    /**
+     * @param string $storeId
+     * @return PaymentSessions
+     * @throws Exception
+     */
+    public function paymentSessions(string $storeId): PaymentSessions
+    {
+        if (!isset($this->paymentSessionsWrappers[$storeId])) {
+            $sdk = $this->getSdk($storeId);
+            $config = $sdk->configuration();
+            $this->paymentSessionsWrappers[$storeId] = new PaymentSessions(
+                $config->getHost(),
+                $sdk->getMerchantId(),
+                $config->getAccessToken(),
+                new Client()
+            );
+        }
+        return $this->paymentSessionsWrappers[$storeId];
+    }
+
+    /**
+     * @param string $storeId
+     * @return PaymentMethodTokens
+     * @throws Exception
+     */
+    public function paymentMethodTokens(string $storeId): PaymentMethodTokens
+    {
+        if (!isset($this->paymentMethodTokensWrappers[$storeId])) {
+            $sdk = $this->getSdk($storeId);
+            $config = $sdk->configuration();
+            $this->paymentMethodTokensWrappers[$storeId] = new PaymentMethodTokens(
+                $config->getHost(),
+                $sdk->getMerchantId(),
+                $config->getAccessToken(),
+                new Client()
+            );
+        }
+        return $this->paymentMethodTokensWrappers[$storeId];
     }
 }
